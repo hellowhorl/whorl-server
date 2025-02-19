b2f2563ad4dbf33cca0c509c9e4c975cdef7392a
dluman <dluman@allegheny.edu>
2025-02-19 11:20:56 -0500

    Adding DELETE endpoint

--- src/persona/urls.py
+++ src/persona/urls.py
@@ -8,4 +8,5 @@ urlpatterns  = [
     path('create/<str:persona_name>', PersonaCreateView.as_view(), name = "persona-create"),
     path('generate/<str:persona_name>', SyncPersonaGenerateView.as_view(), name = "persona-generate"),
     path('cancel/<str:thread_id>', PersonaThreadManagementView.as_view(), name = "persona-thread-cancel"),
+    path('delete/<str:thread_id>', PersonaThreadManagementView.as_view(), name = "persona-thread-delete"),
 ]
