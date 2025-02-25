f3071a2564175d60bdae83bf5d6c475e959fbd9c
wolff01 <wolff01@allegheny.edu>
2025-02-25 09:30:27 -0500

    removed warning

--- src/omnipresence/urls.py
+++ src/omnipresence/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path, re_path
 
 urlpatterns  = [
-    path('/', views.OmnipresenceView.as_view()),
+    path('', views.OmnipresenceView.as_view()),
     re_path(r'update/(?P<pk>\d+)/', views.OmnipresenceUpdateView.as_view()),
     re_path(r'active/', views.OmnipresenceActiveView.as_view()),
     re_path(r'local/', views.OmnipresenceActiveView.as_view())
